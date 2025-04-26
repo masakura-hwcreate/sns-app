cea96d5bca230669b070a7fd0fa709f9fb716877
masakura-hwcreate <masakura@hw-create.com>
2025-04-26 23:21:22 +0900

    create_Post.js

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -2,14 +2,15 @@ import { useContext } from 'react';
 import { SessionContext } from '../SessionProvider';
 
 export function SideMenu() {
+    const { currentUser } = useContext(SessionContext);
     return (
         <div className="bg-white p-4 rounded-lg shadow-md h-[200px] flex flex-col justify-center">
         <h2 className="text-xl font-semibold mb-4">Your Profile</h2>
         <p>
-            <strong>Name:</strong> くるしば
+            <strong>Name:</strong>{currentUser.userName}
         </p>
         <p>
-            <strong>Email:</strong> kuru-shiba@gmail.com
+            <strong>Email:</strong>{currentUser.email}
         </p>
         </div>
     );
